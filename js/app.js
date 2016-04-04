a25085b45dcc0fdc9c3d05f7a45221741e449b7b
Christopher West <9ninemind@gmail.com>
2016-04-03 19:43:32 -0600

    blah

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 define([
     'jquery',
     'underscore',
-    'text!/templates/jobs.html'
-], function($, _, JobTemplate) {
-    var template = _.template(JobTemplate);
+    'text!/templates/jobs.html',
+    'text!/templates/references.html'
+], function($, _, JobTemplate, ReferenceTemplate) {
+    var jobTemplate = _.template(JobTemplate);
+    var referenceTemplate = _.template(ReferenceTemplate);
+
+    _.template.formatDate = function(date) {
+        if (!date) return 'Present';
+
+        var d = new Date(date),
+            fragments = [
+                d.toLocaleString('en-us', { month: "short" }),
+                d.getFullYear()
+            ];
+
+        return fragments.join(' ');
+    };
 
     var initialize = function() {
         $.getJSON('resume.json', function(json) {
-            $('#job-container').html(template({
+            $('#job-container').html(jobTemplate({
                 jobs: json.work
+            }));
+
+            $('#reference-container').html(referenceTemplate({
+                references: json.references
             }));
         });
     };
